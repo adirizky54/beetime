93c7c716b2962e2f4d361c7747bfd85182294253
Adi Rizky <adirizky54@gmail.com>
2026-06-17 17:42:47 +0700

    chore: clean up index.ts, wire health check with @/env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,27 +1,26 @@
-import { showRoutes } from "hono/dev";
+// import { showRoutes } from "hono/dev";
 
-import { createApp } from "@/lib/app";
-import { v1Routes } from "@/routes/v1";
+// import { createApp } from "@/lib/app";
+// import { v1Routes } from "@/routes/v1";
 
-const app = createApp();
+// const app = createApp();
 
-app.route("/v1", v1Routes);
+// app.route("/v1", v1Routes);
 
-showRoutes(app, {
-  verbose: false,
-});
-
-export default app;
-// import { Hono } from "hono";
-// import { env } from "@beetime/env/api";
-
-// const app = new Hono();
-
-// app.get("/", (c) => {
-//   return c.json({
-//     APP_NAME: env.APP_NAME,
-//     BETTER_AUTH_SECRET: env.BETTER_AUTH_SECRET,
-//   });
+// showRoutes(app, {
+//   verbose: false,
 // });
 
 // export default app;
+import { Hono } from "hono";
+import { env } from "@/env";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    APP_NAME: env.APP_NAME,
+  });
+});
+
+export default app;
